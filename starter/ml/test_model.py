197b520c1130a4cab0cea7f2e500f73d127fa168
Joshua Johnson <joshuahjohnson@gmail.com>
2022-07-18 13:30:37 -0600

    Changed import to ml.model and ml.data

--- starter/ml/test_model.py
+++ starter/ml/test_model.py
@@ -1,5 +1,5 @@
-from model import compute_model_metrics, inference
-from data import process_data
+from ml.model import compute_model_metrics, inference
+from ml.data import process_data
 import os
 import joblib
 import numpy as np
